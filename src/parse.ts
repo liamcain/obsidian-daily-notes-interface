80f318a3de5f11ed31bf085088721ec19918fb4d
Liam Cain <hi@liamca.in>
2021-02-11 14:07:56 -0500

    Update parsing to use forgiving mode

--- src/parse.ts
+++ src/parse.ts
@@ -21,6 +21,15 @@ export function getDateUID(
   return `${granularity}-${ts}`;
 }
 
+/**
+ * XXX: When parsing dates that contain both week numbers and months,
+ * Moment choses to ignore the week numbers. For the week dateUID, we
+ * want the opposite behavior. Strip the MMM from the format to patch.
+ */
+export function formatWithoutMonth(format: string): string {
+  return format.replace(/M{1,4}/g, "");
+}
+
 export function getDateFromFile(
   file: TFile,
   granularity: IGranularity
@@ -32,6 +41,13 @@ export function getDateFromFile(
   };
 
   const format = getSettings[granularity]().format.split("/").pop();
+
+  if (granularity === "week") {
+    const weeklyFormat = formatWithoutMonth(format);
+    const noteDate = window.moment(file.basename, weeklyFormat); // forgiving mode
+    return noteDate.isValid() ? noteDate : null;
+  }
+
   const noteDate = window.moment(file.basename, format, true);
   return noteDate.isValid() ? noteDate : null;
 }
