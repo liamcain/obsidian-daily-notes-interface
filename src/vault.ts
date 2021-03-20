034db460e6cf122e6b94bee26e1b3ee5ce9662a7
Liam Cain <hi@liamca.in>
2021-03-20 18:43:06 -0400

    Remove nodejs deps

--- src/vault.ts
+++ src/vault.ts
@@ -1,5 +1,29 @@
 import { normalizePath, App, Notice } from "obsidian";
-import { join } from "path";
+
+// Credit: @creationix/path.js
+function join(...partSegments: string[]) {
+  // Split the inputs into a list of path commands.
+  let parts = [];
+  for (let i = 0, l = partSegments.length; i < l; i++) {
+    parts = parts.concat(partSegments[i].split("/"));
+  }
+  // Interpret the path commands to get the new resolved path.
+  const newParts = [];
+  for (let i = 0, l = parts.length; i < l; i++) {
+    const part = parts[i];
+    // Remove leading and trailing slashes
+    // Also remove "." segments
+    if (!part || part === ".") continue;
+    // Interpret ".." to pop the last segment
+    if (part === "..") newParts.pop();
+    // Push new path segments.
+    else newParts.push(part);
+  }
+  // Preserve the initial slash if there was one.
+  if (parts[0] === "") newParts.unshift("");
+  // Turn back into a single string path.
+  return newParts.join("/") || (newParts.length ? "/" : ".");
+}
 
 async function ensureFolderExists(path: string): Promise<void> {
   const dirs = path.split("/");
